d803e309e73bcb2ef0d354f179ec7c5ca80907c7
yanivzi <yanivzi@checkpoint.com>
2020-05-06 15:32:15 +0300

    validation- start

--- src/Forminput/FormInput.story.tsx
+++ src/Forminput/FormInput.story.tsx
@@ -5,22 +5,59 @@ import { FormInput } from './index'
 storiesOf('Forminput', module)
     .add('Forminput', () => (
         <div style={{padding: 10}}>
+
             <h4>Default</h4>
             <FormInput />
             <br/><hr />
 
-            <h4>Default with label</h4>
 
+            <h4>Default with label</h4>
             <FormInput label={"some label:"} />
             <br/><hr />
+
+
+            <h4>Required</h4>
+            <FormInput label={"some label:"} isRequired />
+            <br/><hr />
+
 
             <h4>Read only</h4>
             <FormInput readOnly value={'lorem ipsum dolor'}/>
             <br/><hr />
 
+
             <h4>Disabled</h4>
             <FormInput disabled value={'lorem ipsum dolor'}/>
             <br/><hr />
+
+
+            <h4>Valid</h4>
+            <FormInput
+                value={'lorem ipsum dolor'}
+                validationMode={'valid'}
+                validationMessage={'Lorem ipsum dolor sit amet'}
+            />
+            <br/><hr />
+
+
+            <h4>Invalid</h4>
+            <FormInput
+                value={'lorem ipsum dolor'}
+                validationMode={'invalid'}
+                validationMessage={'Lorem ipsum dolor sit amet'}
+            />
+            <br/><hr />
+
+
+            <h4>Warning</h4>
+            <FormInput
+                value={'lorem ipsum dolor'}
+                validationMode={'warning'}
+                validationMessage={'Lorem ipsum dolor sit amet'}
+            />
+            <br/><hr />
+
+
 
         </ div>
     ));
--- src/Forminput/FormInput.tsx
+++ src/Forminput/FormInput.tsx
@@ -3,25 +3,168 @@ import { Form } from 'semantic-ui-react'
 import classnames from 'classnames'
 import '../../cp-theme/semantic.less'
 
-const { Input } = Form
+const { Input, Field } = Form
+
+const inputValidationStatus = {
+    NONE: 'none', // default
+    VALID: 'valid',
+    INVALID: 'invalid',
+    WARNING: 'warning',
+};
+
+/*
+validation Obj structure::
+{
+    validationMode: 'valid' || 'invalid' || 'warning',
+    validationMessage: 'some Message..'
+}
+*/
 
 export const FormInput = props => {
     const {
         placeholder,
-        readOnly=false,
-        disabled=false
+        readOnly = false,
+        disabled = false,
+        isRequired = false,
+        validationMode = inputValidationStatus.NONE,
+        validationMessage,
     } = props;
+
     const formTextClasses = classnames({
         'cp-form-input': true,
         'readonly': readOnly,
-        'disabled': disabled
+        'disabled': disabled,
+        'required': isRequired,
+        'has-validation valid': validationMode === inputValidationStatus.VALID,
+        'has-validation invalid': validationMode === inputValidationStatus.INVALID,
+        'has-validation warning': validationMode === inputValidationStatus.WARNING
     });
 
     return (
+        <>
             <Input
                 className={formTextClasses}
                 placeholder={placeholder || 'Enter text here...'}
                 {...props}
             />
+            { validationMode !== inputValidationStatus.NONE && validationMessage &&
+                <span className={'validation-message'}>{validationMessage}</span> }
+        </>
+
     )
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* import React, {useState} from 'react'
+import classNames from 'classnames/bind';
+import Icon from 'front-common-icons'
+import { Input } from 'antd'
+import 'antd/dist/antd.less'
+
+const { Search } = Input
+
+const inputValidationStatus = {
+    NONE: 'none', // default
+    VALID: 'valid',
+    INVALID: 'invalid',
+    WARNING: 'warning',
+};
+
+export const FormInput = (props) => {
+    const {
+        placeholder,
+        readOnly = false,
+        disabled = false,
+        isMandatory = false,
+        isSearchInput = false,
+        onChange
+    } = props;
+    const [inputValue, setInputValue] = useState('');
+
+    const shouldDisplayValidationState = !readOnly && !disabled;
+
+    const formInputClass = classNames({
+        'cp-form-input': true,
+        'mandatory': isMandatory,
+        'readonly': readOnly,
+        'disabled': disabled,
+        'search': isSearchInput,
+        'has-value': inputValue !== ''
+    });
+
+    function handleChange(e) {
+        setInputValue(e.target.value);
+        onChange && onChange(e)
+    }
+
+    function handleDeleteIconClick() {
+        setInputValue('')
+    }
+
+    function renderSuffixIcon() {
+        return <Icon
+            className={"icon icon-delete"}
+            name={'cancel-circle'}
+            width={14}
+            height={14}
+            color={'#B3AA6A'}
+            onClick={handleDeleteIconClick}
+        />;
+    }
+
+    function renderSearchIcon() {
+        return <Icon className={'icon icon-search'} name={'search'} width={14} height={14} />
+    }
+    return isSearchInput ? (
+        <Search
+            className={formInputClass}
+            value={inputValue}
+            onChange={handleChange}
+            placeholder={placeholder || 'Search..'}
+            prefix={renderSearchIcon()}
+            suffix={inputValue && renderSuffixIcon()}
+        />
+    ) : (
+        <Input
+            className={formInputClass}
+            placeholder={placeholder || 'Type here..'}
+            value={inputValue}
+            onChange={handleChange}
+            prefix={isSearchInput && renderSearchIcon()}
+            {...props}
+        />
+
+    )
+};
+*/
