fe94b517237fc74323a3349221c18e13e2aede08
yanivzi <yanivzi@checkpoint.com>
2020-05-10 14:18:10 +0300

    search- done for now

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Search as SemanticSearch, Input as SInput } from 'semantic-ui-react'
-import classnames from 'classnames'
 import '../../cp-theme/semantic.less'
 
 
