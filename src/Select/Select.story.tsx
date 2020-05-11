220dd24b9b5acfdfc63edc101083e81ececa4051
Anna Penchansky <annapenchansky@Annas-MacBook-Pro.local>
2020-05-11 10:33:57 +0300

    will pull master to see icon changes

--- src/Select/Select.story.tsx
+++ src/Select/Select.story.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Select } from './index'
-// import '../../cp-theme/semantic.less'
 
-const options = [
-	{ key: 1, text: 'Choice 1', value: 1 },
-	{ key: 2, text: 'Choice 2', value: 2 },
-	{ key: 3, text: 'Choice 3', value: 3 },
-]
-
-console.log('here')
 storiesOf('Select', module)
 	.add('Select', () => (
 		<div style={{ padding: 10 }}>
-			<p>{' '}First Select</p>
+			<p>{' '}Combo Default</p>
 			<Select></Select>
 			<hr />
+			<p>Default Disable</p>
+			<Select disabled></Select>
+			<hr />
+			<p>Default Invalid</p>
+			<Select invalid></Select>
+			<hr />
+			<p>{' '}Combo Box</p>
+			<Select styleType={'snow'}></Select>
+
 		</div>
 	))
--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,44 +1,7 @@
-// import * as React from 'react'
-// import { Menu, Dropdown as SDropdown, DropdownProps, Select } from 'semantic-ui-react'
-// import classnames from 'classnames'
-// import '../../cp-theme/semantic.less'
-
-
-// const options = [
-// 	{ key: 1, text: 'Choice 1', value: 1 },
-// 	{ key: 2, text: 'Choice 2', value: 2 },
-// 	{ key: 3, text: 'Choice 3', value: 3 },
-// ]
-
-// interface InternalDropdownProps extends DropdownProps {
-// 	// onClick?: { func }
-// 	// onChange?: { func }
-// 	styleType?: 'selected'
-// }
-
-// export const Dropdown = (props: InternalDropdownProps) => {
-// 	const {
-// 		styleType,
-// 		options,
-// 		...rest
-// 	} = props;
-// 	const className = classnames('cp-dropdown', {})
-// 	return (
-// 		<Menu compact className={'dropdown-menu'}>
-// 			<SDropdown
-// 				selection
-// 				text='Dropdown' options={options}
-// 				className={className}
-// 				{...rest}
-// 			/>
-// 		</Menu>
-// 	)
-// }
-
 import * as React from 'react'
 import { Select as SSelect, SelectProps } from 'semantic-ui-react'
 import classnames from 'classnames'
-// import '../../cp-theme/semantic.less'
+import '../../cp-theme/semantic.less'
 
 const options = [
 	{ key: 1, text: 'Choice 1', value: 1 },
@@ -46,25 +9,21 @@ const options = [
 	{ key: 3, text: 'Choice 3', value: 3 },
 ]
 
-interface InternalSelectProps extends SelectProps {
-	styleType?: 'selected'
-}
-
-export const Select = () => {
-	// const {
-	// 	styleType,
-	// 	...rest
-	// } = props;
-	const className = classnames('cp-dropdown', {})
+export const Select = (props) => {
+	const {
+		invalid,
+		readOnly,
+		styleType = 'default',
+		...rest
+	} = props;
+	const className = classnames('cp-select', styleType, { invalid })
 	return (
-		// <Menu compact className={'dropdown-menu'}>
 		<SSelect
+			placeholder={'Select Option'}
+			className={className}
 			options={options}
-		// fluid selection options={options}
-		// text='Select'
-		// className={className}
-		// {...rest}
+			readOnly={false}
+			{...rest}
 		/>
-		// </Menu>
 	)
 }
