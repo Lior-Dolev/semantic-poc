b6e6f5b845cf58c5c6ff4486e33a8e440d14308d
yanivzi <yanivzi@checkpoint.com>
2020-05-13 19:05:05 +0300

    chips cont

--- src/Chip/Chip.story.tsx
+++ src/Chip/Chip.story.tsx
@@ -22,7 +22,7 @@ storiesOf('Semantic', module)
 
 function ExampleContainerComponent(props) {
     const chipsExampleObj = {
-        chip11: true,
+        chisaljdasljdlkjaskldjaslkjdlkajsklsjadlkajsp11: true,
         chip22: false,
         chip33: false,
         chip44: true,
